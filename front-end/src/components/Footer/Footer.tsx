f0aca88121d6624d967d8e9ea7a64ac94ac2ec4b
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-19 12:54:58 +0100

    footer

---
+++ front-end/src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-default flex h-50 w-full items-center justify-center">
+            <p className="text-white">Copyright 2026</p>
+        </footer>
+    );
+};
