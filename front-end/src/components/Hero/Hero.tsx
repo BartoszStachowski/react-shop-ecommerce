b0bec448646c48f832c9524676e6a6eaee4c09cd
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-20 14:02:04 +0100

    add hero component

---
+++ front-end/src/components/Hero/Hero.tsx
@@ -0,0 +1,19 @@
+import { CenteredContent } from '@/components/CenteredContent/CenteredComponent.tsx';
+import { FullWidthButton } from '@/components/FullWidthButton/FullWidthButton.tsx';
+
+export const Hero = ({ heroImage }: { heroImage: string }) => {
+    return (
+        <div
+            style={{ backgroundImage: `url(${heroImage})` }}
+            className="h-125 w-full bg-cover"
+        >
+            <CenteredContent>
+                <div className="mt-20 inline-block bg-white/80 px-20 py-12">
+                    <h2>Summer sale – up to 70% off</h2>
+                    <p className="mt-4 mb-8">Only the best deals</p>
+                    <FullWidthButton>Sprawdź produkty</FullWidthButton>
+                </div>
+            </CenteredContent>
+        </div>
+    );
+};
