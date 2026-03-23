547364646d683da9e69683278b119cbb6cfb7e2b
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-23 14:59:32 +0100

    subcategories

--- front-end/src/constants/categories.ts
+++ front-end/src/constants/categories.ts
@@ -5,8 +5,72 @@ export const GENDERS = [
 ];
 
 export const CATEGORIES = [
-    { categoryName: 'Clothing', path: 'clothing' },
-    { categoryName: 'Shoes', path: 'shoes' },
-    { categoryName: 'Accessories', path: 'accessories' },
-    { categoryName: 'Sport', path: 'sport' },
+    {
+        categoryName: 'Clothing',
+        path: 'clothing',
+        subcategories: [
+            {
+                categoryName: 'T-Shirts',
+                path: 't-shirts',
+            },
+            {
+                categoryName: 'Sweaters',
+                path: 'sweaters',
+            },
+            {
+                categoryName: 'Pants',
+                path: 'pants',
+            },
+        ],
+    },
+    {
+        categoryName: 'Shoes',
+        path: 'shoes',
+        subcategories: [
+            {
+                categoryName: 'Formal',
+                path: 'formal',
+            },
+            {
+                categoryName: 'Sports',
+                path: 'sports',
+            },
+            {
+                categoryName: 'Sneakers',
+                path: 'sneakers',
+            },
+        ],
+    },
+    {
+        categoryName: 'Accessories',
+        path: 'accessories',
+        subcategories: [
+            {
+                categoryName: 'Bags',
+                path: 'bags',
+            },
+            {
+                categoryName: 'Watches',
+                path: 'watches',
+            },
+        ],
+    },
+    {
+        categoryName: 'Sport',
+        path: 'sport',
+        subcategories: [
+            {
+                categoryName: 'Football',
+                path: 'football',
+            },
+            {
+                categoryName: 'Skiing',
+                path: 'skiing',
+            },
+            {
+                categoryName: 'Swimming',
+                path: 'swimming',
+            },
+        ],
+    },
 ];
