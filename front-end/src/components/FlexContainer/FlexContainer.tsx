06d65b86b6f5bb90b60a7b1a10567033df27fecb
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-23 15:00:52 +0100

    add class to FlexContainer component

--- front-end/src/components/FlexContainer/FlexContainer.tsx
+++ front-end/src/components/FlexContainer/FlexContainer.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
 export const FlexContainer = ({ children }: { children: React.ReactNode }) => {
-    return <div className="flex w-full max-w-400 gap-4">{children}</div>;
+    return (
+        <div className="mx-auto my-0 flex w-full max-w-400 items-start gap-4">
+            {children}
+        </div>
+    );
 };
