7b25e4cb65dc75a2baa9d1d79bf51fae00a4516d
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-23 15:00:20 +0100

    rename Bestsellers to Products

--- front-end/src/components/Products/Products.tsx
+++ front-end/src/components/Products/Products.tsx
@@ -2,11 +2,16 @@ import type { ProductType } from '@/types/product.ts';
 import { CenteredContent } from '@/components/CenteredContent/CenteredComponent.tsx';
 import { Product } from '@/Product/Product.tsx';
 
-export const Bestsellers = ({ products }: { products: ProductType[] }) => {
+interface ProductsProps {
+    products: ProductType[];
+    headerText: string;
+}
+
+export const Products = ({ products, headerText }: ProductsProps) => {
     return (
         <CenteredContent>
-            <h2 className="mt-8 mb-4">Explore Popular Picks</h2>
-            <div className="flex justify-between">
+            <h2 className="mt-8 mb-4">{headerText}</h2>
+            <div className="flex flex-wrap justify-between">
                 {products.map((product: ProductType) => (
                     <Product key={product.id} product={product} />
                 ))}
