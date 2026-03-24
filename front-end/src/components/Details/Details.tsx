623e25c62bc7c19bd6cbb98c67c59084823766e0
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-24 11:07:43 +0100

    added Details component

---
+++ front-end/src/components/Details/Details.tsx
@@ -0,0 +1,48 @@
+import CAR_ICON from '@/assets/car.svg';
+import RETURN_ICON from '@/assets/return.svg';
+import type { ProductType } from '@/types/product.ts';
+import { FullWidthButton } from '@/components/FullWidthButton/FullWidthButton.tsx';
+import { Accordion } from '@/components/Accordion/Accordion.tsx';
+
+export const Details = ({ product }: { product: ProductType }) => {
+    const accordionContent = [
+        {
+            title: 'Product Description',
+            content: product.description,
+        },
+        {
+            title: 'Care Instructions',
+            content: product.description,
+        },
+    ];
+
+    return (
+        <div className="w-full pr-4">
+            <h2>{product.brand}</h2>
+            <p className="my-2 font-bold">{product.productName}</p>
+            <p className="mb-8 text-2xl text-red-700">{product.pricePLN}zł</p>
+            <FullWidthButton isBlack={true}>Dodaj do koszyka</FullWidthButton>
+
+            <ul className="mt-8">
+                <li className="mb-8 flex items-center justify-start gap-4">
+                    <img
+                        src={CAR_ICON}
+                        alt="delivery icon"
+                        className="h-4 w-4"
+                    />
+                    Delivery within 24h
+                </li>
+                <li className="mb-8 flex items-center justify-start gap-4">
+                    <img
+                        src={RETURN_ICON}
+                        alt="return icon"
+                        className="h-4 w-4"
+                    />
+                    Returns within 100 days!
+                </li>
+            </ul>
+
+            <Accordion items={accordionContent} />
+        </div>
+    );
+};
