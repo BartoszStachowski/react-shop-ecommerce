10891c18437e946975676fb7251cca50ef7b0119
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-25 15:07:02 +0100

    rwd

--- front-end/src/components/product/Products/Products.tsx
+++ front-end/src/components/product/Products/Products.tsx
@@ -11,7 +11,7 @@ export const Products = ({ products, headerText }: ProductsProps) => {
     return (
         <CenteredContent>
             <h2 className="mt-8 mb-4">{headerText}</h2>
-            <div className="flex flex-wrap justify-between">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
                 {products.map((product: ProductType) => (
                     <Product key={product.id} product={product} />
                 ))}
