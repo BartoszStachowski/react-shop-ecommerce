2426ea69248cc399f16f3dc1405bb90ed5341a5d
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-03-26 10:02:18 +0100

    breadcrumbs

--- front-end/src/components/navigation/Breadcrumbs/Breadcrumbs.tsx
+++ front-end/src/components/navigation/Breadcrumbs/Breadcrumbs.tsx
@@ -1,27 +1,46 @@
 import ARROW_ICON from '@/assets/arrow.svg';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import clsx from 'clsx';
+import { CATEGORIES, GENDERS } from '@/constants/categories.ts';
+
+type BreadcrumbItem = {
+    categoryName: string;
+    path: string;
+};
 
 export const Breadcrumbs = () => {
-    const breadcrumbs = [
-        {
-            categoryName: 'Women',
-            path: 'woman',
+    const { gender, category, subcategory } = useParams();
+
+    const foundGender = GENDERS.find((g) => g.path === gender);
+    const foundCategory = CATEGORIES.find((c) => c.path === category);
+    const foundSubcategory = foundCategory?.subcategories.find(
+        (sc) => sc.path === subcategory
+    );
+
+    const breadcrumbs: BreadcrumbItem[] = [
+        foundGender && {
+            categoryName: foundGender.categoryName,
+            path: `/${foundGender.path}`,
         },
-        {
-            categoryName: 'Clothing',
-            path: 'clothing',
-        },
-        {
-            categoryName: 'Sweaters',
-            path: 'sweaters',
-        },
-    ];
+        foundGender &&
+            foundCategory && {
+                categoryName: foundCategory.categoryName,
+                path: `/${foundGender.path}/${foundCategory.path}`,
+            },
+        foundGender &&
+            foundCategory &&
+            foundSubcategory && {
+                categoryName: foundSubcategory.categoryName,
+                path: `/${foundGender.path}/${foundCategory.path}/${foundSubcategory.path}`,
+            },
+    ].filter(Boolean) as BreadcrumbItem[]; // removes all falsy values
+
     return (
         <ul className="mt-4 mb-8 flex items-center justify-start gap-2">
             {breadcrumbs.map((breadcrumb) => (
                 <li key={breadcrumb.path} className="last:[&_img]:hidden">
                     <NavLink
+                        end
                         to={breadcrumb.path}
                         className={({ isActive }) =>
                             clsx('flex text-xs uppercase', {
